6bbb6ce804e9a124f26a87692a28fbae3b786974
Daniel Wilson <stringofbeans@gmail.com>
2020-12-08 16:07:56 -0800

    body-parser intialization and mongoose connection

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 var express = require('express');
 var cors = require('cors');
+const mongoose = require('mongoose'),
+  multer = require('multer'),
+  bodyParser = require('body-parser');
 require('dotenv').config()
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({extended: false}));
+mongoose.connect(process.env.DB_URI, {useFindAndModify: false, useNewUrlParser: true});
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
+
 
 
 
